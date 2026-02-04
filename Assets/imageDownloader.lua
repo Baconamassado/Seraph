735d6e607569485d335160e4923d8b2486b7695f
Bacon <149910312+Baconamassado@users.noreply.github.com>
2026-02-04 18:24:31 -0300

    Alterar os comentarios

--- Assets/imageDownloader.lua
+++ Assets/imageDownloader.lua
@@ -1,4 +1,5 @@
 -- Provavelmente vai ser subistituido pelo loading (que vai dar um proposito pra ele em vez de só demorar mais pra carregar)
+-- realmente subistituido pelo loading, mas vou deixar ai pra possiveis utilidades futuras
 
 
 local function baixarIm(url, f, customPath)
